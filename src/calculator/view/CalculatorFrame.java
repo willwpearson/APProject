66ac7d28b9a6d4b95cd600a0f1a41a6e4184354f
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-07 12:22:21 -0700

    Wrote the frame setup.

--- src/calculator/view/CalculatorFrame.java
+++ src/calculator/view/CalculatorFrame.java
@@ -1,6 +1,32 @@
 package calculator.view;
 
+import calculator.controller.CalculatorController;
+import javax.swing.JFrame;
+
 public class CalculatorFrame
 {
-
+	private CalculatorController appController;
+	private CalculatorPanel appPanel;
+	
+	public CalculatorFrame(CalculatorController appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new CalculatorPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("Calculator");
+		this.setResizeable(false);
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
+	
+	public CalculatorController getAppController()
+	{
+		return appController;
+	}
 }
