864f8da35e9a8ee3d7fb246d1c136e7eefbf3240
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-07 12:17:42 -0700

    Basic calculator method framework.

--- src/calculator/model/Calculator.java
+++ src/calculator/model/Calculator.java
@@ -1,6 +1,45 @@
 package calculator.model;
 
 public class Calculator
-{
-
+{	
+	public Calculator()
+	{
+		
+	}
+	
+	public int addMethod(int first, int second)
+	{
+		int sum = 0;
+		
+		sum = first + second;
+		
+		return sum;
+	}
+	
+	public int subtractMethod(int first, int second)
+	{
+		int difference = 0;
+		
+		difference = first - second;
+		
+		return difference;
+	}
+	
+	public int mulitplyMethod(int first, int second)
+	{
+		int product = 0;
+		
+		product = first * second;
+		
+		return product;
+	}
+	
+	public int divideMethod(int first, int second)
+	{
+		int quotient = 0;
+		
+		quotient = first / second;
+		
+		return quotient;
+	}
 }
