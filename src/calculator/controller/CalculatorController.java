d5af2c0d35d19160b0b4ed1968f1a4a94549c4db
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-07 12:07:54 -0700

    Framework runner and controller code.

--- src/calculator/controller/CalculatorController.java
+++ src/calculator/controller/CalculatorController.java
@@ -1,6 +1,21 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.view.CalculatorFrame;
+
 public class CalculatorController
 {
-
+	private CalculatorFrame appFrame;
+	private Calculator appCalculator;
+	
+	public CalculatorController()
+	{
+		appFrame = new CalculatorFrame();
+		appCalculator = new Calculator();
+	}
+	
+	public void start()
+	{
+		
+	}
 }
--- src/calculator/controller/CalculatorRunner.java
+++ src/calculator/controller/CalculatorRunner.java
@@ -2,5 +2,9 @@ package calculator.controller;
 
 public class CalculatorRunner
 {
-
+	public static void main(String [] args)
+	{
+		CalculatorController app = new CalculatorController();
+		app.start();
+	}
 }
